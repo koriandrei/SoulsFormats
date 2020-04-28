15bf9c2dbd3c6b5702b2f330be3a1a816edc2143
dmitrii.kozlov <dmitrij.kozlov.h@gmail.com>
2020-04-28 16:18:10 +0300

    Moved parts of root motion code to formats

--- SoulsFormats/Formats/HKX/HavokAnimationData.cs
+++ SoulsFormats/Formats/HKX/HavokAnimationData.cs
@@ -51,6 +51,145 @@ namespace SoulsFormats.Havok
         }
     }
 
+
+    public class NewRootMotionHandlerData
+    {
+        //public Matrix CurrentAbsoluteRootMotion = Matrix.Identity;
+
+        public readonly Vector4 Up;
+        public readonly Vector4 Forward;
+        public readonly float Duration;
+        public readonly Vector4[] Frames;
+
+        /// <summary>
+        /// The accumulative root motion delta applied by playing the entire anim from the beginning to the end.
+        /// </summary>
+        public readonly Vector4 LoopDeltaForward;
+
+        /// <summary>
+        /// The accumulative root motion delta applied by playing the entire anim in reverse from the end to the beginning.
+        /// </summary>
+        public readonly Vector4 LoopDeltaBackward;
+
+
+        public NewRootMotionHandlerData(Vector4 up, Vector4 forward, float duration, Vector4[] frames)
+        {
+            Up = up;
+            Forward = forward;
+            Duration = duration;
+            Frames = frames;
+
+            LoopDeltaForward = frames[frames.Length - 1] - frames[0];
+            LoopDeltaBackward = frames[0] - frames[frames.Length - 1];
+        }
+
+
+        public bool Accumulate = true;
+
+        private Matrix4x4 GetMatrixFromSample(Vector4 sample)
+        {
+            return Matrix4x4.CreateRotationY(sample.W) *
+                Matrix4x4.CreateWorld(
+                    new Vector3(sample.X, sample.Y, sample.Z),
+                    new Vector3(Forward.X, Forward.Y, -Forward.Z),
+                    new Vector3(Up.X, Up.Y, Up.Z));
+        }
+
+        private Vector4 GetSample(float frame)
+        {
+            float frameFloor = (float)Math.Floor(frame % (Frames.Length - 1));
+            Vector4 sample = Frames[(int)frameFloor];
+
+            if (frame != frameFloor)
+            {
+                float frameMod = frame % 1;
+
+                Vector4 nextFrameRootMotion;
+
+                //if (frame >= Frames.Length - 1)
+                //    nextFrameRootMotion = Frames[0];
+                //else
+                //    nextFrameRootMotion = Frames[(int)(frameFloor + 1)];
+
+                nextFrameRootMotion = Frames[(int)(frameFloor + 1)];
+
+                sample = Vector4.Lerp(sample, nextFrameRootMotion, frameMod);
+            }
+
+            return sample;
+        }
+
+        private static Vector4 AddRootMotion(Vector4 start, Vector4 toAdd, float direction, bool dontAddRotation = false)
+        {
+            if (!dontAddRotation)
+                start.W += toAdd.W;
+
+            Vector3 displacement = Vector3.Transform(new Vector3(toAdd.X, toAdd.Y, toAdd.Z), Matrix4x4.CreateRotationY(direction));
+            start.X += displacement.X;
+            start.Y += displacement.Y;
+            start.Z += displacement.Z;
+            return start;
+        }
+
+        public (Vector4 Motion, float Direction) UpdateRootMotion(Vector4 currentRootMotion, Vector4 prevFrameData, float currentDirection, float currentFrame, int loopCountDelta, bool forceAbsoluteRootMotion)
+        {
+            if (forceAbsoluteRootMotion)
+                return (currentRootMotion, currentDirection);
+
+            var nextFrameData = GetSample(currentFrame);
+
+            if (Accumulate)
+            {
+                //currentRootMotion *= GetMatrixFromSample(nextFrameData - prevFrameData);
+
+                currentRootMotion = AddRootMotion(currentRootMotion, nextFrameData - prevFrameData, currentDirection, dontAddRotation: true);
+
+                currentRootMotion.W = nextFrameData.W;
+
+                while (loopCountDelta != 0)
+                {
+                    if (loopCountDelta > 0)
+                    {
+                        currentRootMotion = AddRootMotion(currentRootMotion, LoopDeltaForward, currentDirection, dontAddRotation: true);
+                        currentDirection += LoopDeltaForward.W;
+                        loopCountDelta--;
+                    }
+                    else if (loopCountDelta < 0)
+                    {
+                        currentRootMotion = AddRootMotion(currentRootMotion, LoopDeltaBackward, currentDirection, dontAddRotation: true);
+                        currentDirection += LoopDeltaBackward.W;
+                        loopCountDelta++;
+                    }
+                }
+            }
+            else
+            {
+                currentDirection = 0;
+                currentRootMotion = AddRootMotion(Vector4.Zero, nextFrameData, currentDirection);
+            }
+
+            // TODO: This code remains of what was in DSAnimStudio.
+            // Maybe remove it
+            // prevFrameData = nextFrameData;
+
+            //CurrentAbsoluteRootMotion = GetMatrixFromSample(GetSample(currentFrame));
+
+            //if (forceAbsoluteRootMotion)
+            //{
+
+            //}
+            //else
+            //{
+            //    CurrentAbsoluteRootMotion *= GetMatrixFromSample(nextFrameData - prevFrameData);
+            //}
+
+
+
+            return (currentRootMotion, currentDirection);
+        }
+    }
+
+
     public abstract class HavokAnimationData
     {
         public HKX.AnimationBlendHint BlendHint = HKX.AnimationBlendHint.NORMAL;
